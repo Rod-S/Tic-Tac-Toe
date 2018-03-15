3b252562bf28321981145470de30e8aa4a2762ad
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-03-15 14:53:08 -0500

    -board start functionality added -X and O toggle functionality added (unfinished)

--- tic-tac-toe-v3/javascript/game.js
+++ tic-tac-toe-v3/javascript/game.js
@@ -1,17 +1,34 @@
-(function (){
+(function ticTacToe(){
 
+  $('#start').show();
   $('#board').hide();
+  $('#finish').hide();
 
-  let user1;
-  let user2;
+  let player_1;
+  let player_2;
 
   $('#startButton').on('click', function (){
-    user1 = prompt('Please enter a name', 'Player 1');
+    player_1 = prompt('Please enter a name for player 1', 'player_1');
+    player_2 = prompt('Please enter a name for player 2', 'player_2');
     $('#start').hide();
     $('#board').show();
     $('#player1').addClass('active');
   });
 
+  $('.box').on('click', function (event) {
+    $('#player1').toggleClass('active');
+    $('#player2').toggleClass('active');
+
+    if ($('#player1').is('.active')) {
+      $(this).toggleClass('box-filled-2');
+    } else if ($('#player2').is('.active')){
+      $(this).toggleClass('box-filled-1');
+    }
+  });
+
+  $('.box').on('hover', function (event) {
+
+  });
 
 
 }());
